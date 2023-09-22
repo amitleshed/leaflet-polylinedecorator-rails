54aae09986a3a1448b1542ba8ec3f3210cbd9cee
Amit Leshed <amitleshed@icloud.com>
2023-09-22 14:37:35 -0400

    Create leaflet-markercluster-rails.rb

---
+++ lib/leaflet-markercluster-rails.rb
@@ -0,0 +1,10 @@
+require "leaflet-polylinedecorator-rails/version"
+
+module Leaflet
+  module PolylineDecorator
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
